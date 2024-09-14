468d20eff936a23944bb0866343e0a291ea45402
sunghwan <ghktkf789@gmail.com>
2024-09-15 01:35:14 +0900

    feat : frist Commit - HomeController

---
+++ src/main/java/com/ksh/matzips/MatzipsApplication.java
@@ -0,0 +1,13 @@
+package com.ksh.matzips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatzipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatzipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksh/matzips/MatzipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksh.matzips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatzipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
