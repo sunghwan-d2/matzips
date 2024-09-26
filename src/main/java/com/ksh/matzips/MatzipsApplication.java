3db420e8f5456054b7f12c73781173673ab297c1
sunghwan <ghktkf789@gmail.com>
2024-09-26 20:12:41 +0900

    feat: 9.26 commit

--- src/main/java/com/ksh/matzips/MatzipsApplication.java
+++ src/main/java/com/ksh/matzips/MatzipsApplication.java
@@ -2,10 +2,10 @@ package com.ksh.matzips;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MatzipsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MatzipsApplication.class, args);
 	}
--- src/main/java/com/ksh/matzips/regexes/EmailAuthRegex.java
+++ src/main/java/com/ksh/matzips/regexes/EmailAuthRegex.java
@@ -5,4 +5,6 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class EmailAuthRegex {
     public static final Regex email = new Regex(UserRegex.email.expression);
+    public static final Regex code = new Regex("^(\\d{6})$");
+    public static final Regex salt = new Regex("^([\\da-f]{128})$");
 }
